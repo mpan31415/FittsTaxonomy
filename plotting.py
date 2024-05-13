6aadde1db843ff31588f8fb3dc5e1d7c232eef6d
mpan31415 <michael.pan31415@gmail.com>
2024-05-14 09:00:36 +1000

    plotting

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 from pandas import read_csv
 from os import getcwd
+import matplotlib.pyplot as plt
 
 
 ##################################################
@@ -7,7 +8,6 @@ def read_data(part_id, task_id, trial_id, operation_type):
     
     csv_dir = getcwd()+"/user_study_results/cleaned_traj/"+operation_type+"/part"+str(part_id)+"-task"+str(task_id)+"-t"+str(trial_id)+".csv"
     raw_df = read_csv(csv_dir)
-    raw_df = raw_df.iloc[: , 1:]
     
     return raw_df
 
@@ -15,9 +15,19 @@ def read_data(part_id, task_id, trial_id, operation_type):
 ##################################################
 def main():
     
-    df = read_data(1, 1, 1, "kt")
-    print(df)
+    df = read_data(6, 3, 1, "teleop")
     
+    # get translation points across the traj
+    txs = df['tx']
+    tys = df['ty']
+    tzs = df['tz']
+    
+    # plot
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.scatter(txs, tys, tzs, label='trajectory')
+    ax.view_init(elev=15., azim=20, roll=0)
+    
+    plt.show()
     
     
     
