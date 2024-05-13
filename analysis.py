2a9c45d0c6a6c2efdd08185c4fbcded01755185f
mpan31415 <michael.pan31415@gmail.com>
2024-05-13 12:07:15 +0000

    clean all traj data

---
+++ analysis.py
@@ -0,0 +1,121 @@
+from panda_fk import get_chain, compute_fk
+from math import pi
+from pandas import read_csv, DataFrame
+from os import getcwd
+from re import search, IGNORECASE
+from ast import literal_eval
+
+
+NUM_PARTICIPANTS = 10
+
+
+##################################################
+def clean_data(part_id, task_id, trial_id, operation_type, panda_chain):
+    
+    csv_dir = getcwd()+"/FittsTaxonomy/user_study_results/trajectory/" + operation_type + "/task"+str(task_id)+"-"+str(trial_id)+"-"+str(part_id)+".csv"
+    raw_df = read_csv(csv_dir)
+    
+    # raw (sec, nanosec)
+    raw_sec_list = raw_df['sec'].tolist()
+    raw_nanosec_list = raw_df['nanosec'].tolist()
+    
+    # get times from start (in seconds) and store into a single list
+    raw_sec_start = raw_sec_list[0]
+    raw_nanosec_start = raw_nanosec_list[0]
+    sec_list = [raw_sec_list[i] - raw_sec_start for i in range(len(raw_sec_list))]
+    nanosec_list = [round((raw_nanosec_list[i] - raw_nanosec_start)/1e9, 4) for i in range(len(raw_nanosec_list))]
+    times_from_start = [round(sec_list[i]+nanosec_list[i], 3) for i in range(len(sec_list))]
+    
+    # get joint positions and forward kinematics and store into separate lists
+    gr_list = []
+    
+    tx_list = []
+    ty_list = []
+    tz_list = []
+    
+    qw_list = []
+    qx_list = []
+    qy_list = []
+    qz_list = []
+    
+    rx_list = []
+    ry_list = []
+    rz_list = []
+    
+    positions = raw_df['positions'].tolist()
+    
+    # loop through all recorded positions
+    for pos_vec in positions:
+        
+        joint_positions = literal_eval(search("array\('d'\, (.*)\)", pos_vec, IGNORECASE).group(1))
+        
+        gr_list.append(joint_positions[7])
+        
+        # remove the last two elements from the joint positions list (they are the gripper values which have already been saved)
+        joint_positions.pop(-1)
+        joint_positions.pop(-1)
+        
+        # calculate forward kinematics
+        trans, rot_mat, quat, euler = compute_fk(joint_positions, panda_chain)
+        
+        tx_list.append(trans[0])
+        ty_list.append(trans[1])
+        tz_list.append(trans[2])
+        
+        qw_list.append(quat[0])
+        qx_list.append(quat[1])
+        qy_list.append(quat[2])
+        qz_list.append(quat[3])
+        
+        rx_list.append(euler[0])
+        ry_list.append(euler[1])
+        rz_list.append(euler[2])
+        
+    df_dict = {
+        "time": times_from_start,
+        "gr": gr_list,
+        "tx": tx_list,
+        "ty": ty_list,
+        "tz": tz_list,
+        "rx": rx_list,
+        "ry": ry_list,
+        "rz": rz_list,
+        "qw": qw_list,
+        "qx": qx_list,
+        "qy": qy_list,
+        "qz": qz_list
+    }
+    new_df = DataFrame(df_dict)
+    
+    return new_df
+
+
+
+##################################################
+def main():
+    
+    panda_chain = get_chain()
+    
+    ###### clean all data for kinesthetic teaching ######
+    for part_id in range(1, NUM_PARTICIPANTS+1):
+        for task_id in range(1, 4):
+            for trial_id in range(1, 4):
+                data = clean_data(part_id, task_id, trial_id, "kinesthetic", panda_chain)
+                dest_dir = getcwd()+"/FittsTaxonomy/user_study_results/cleaned_traj/kt/part"+str(part_id)+"-task"+str(task_id)+"-t"+str(trial_id)+".csv"
+                data.to_csv(dest_dir)
+    
+    ###### clean all data for teleoperation ######
+    for part_id in range(1, NUM_PARTICIPANTS+1):
+        for task_id in range(1, 4):
+            for trial_id in range(1, 4):
+                data = clean_data(part_id, task_id, trial_id, "teleoperation", panda_chain)
+                dest_dir = getcwd()+"/FittsTaxonomy/user_study_results/cleaned_traj/teleop/part"+str(part_id)+"-task"+str(task_id)+"-t"+str(trial_id)+".csv"
+                data.to_csv(dest_dir)
+
+    print("\n\n Finished cleaning all data!! \n\n")
+    
+    
+
+##################################################
+if __name__ == "__main__":
+    main()
--- panda_fk.py
+++ panda_fk.py
@@ -1,9 +1,10 @@
 import PyKDL
 from urdf_parser_py.urdf import URDF
 import kdl_parser_py.urdf
-from math import pi
 from numpy import array
-
+from os import getcwd
+from math import pi
+from math import atan2, asin
 
 ###################################################
 def load_robot_model(urdf_file):
@@ -19,7 +20,7 @@ def load_robot_model(urdf_file):
 
 ###################################################
 def get_chain():
-    urdf_file = 'panda.urdf'
+    urdf_file = getcwd() + '/FittsTaxonomy/panda.urdf'
     # Load the robot model
     tree = load_robot_model(urdf_file)
     # Get the chain for the robot arm
@@ -48,36 +49,63 @@ def compute_forward_kinematics(chain, joint_angles):
     return end_frame
 
 ###################################################
+def euler_from_quaternion(w, x, y, z):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = atan2(t3, t4)
+     
+        return [roll_x, pitch_y, yaw_z] # in radians
+
+###################################################
 def compute_fk(joint_positions, chain):
     # Compute the forward kinematics
     end_frame = compute_forward_kinematics(chain, joint_positions)
     translation = end_frame.p
     rotation = end_frame.M
     trans = [translation.x(), translation.y(), translation.z()]
+    trans = [round(n, 5) for n in trans]
     rot_mat = array([[rotation[0,0], rotation[0,1], rotation[0,2]],
-                             [rotation[1,0], rotation[1,1], rotation[1,2]],
-                             [rotation[2,0], rotation[2,1], rotation[2,2]]])
+                     [rotation[1,0], rotation[1,1], rotation[1,2]],
+                     [rotation[2,0], rotation[2,1], rotation[2,2]]])
     quaternion = rotation.GetQuaternion()
     # Store as numpy array or list (depending on your preference)
-    quat = array([quaternion[0], quaternion[1], quaternion[2], quaternion[3]])
+    quat = [quaternion[0], quaternion[1], quaternion[2], quaternion[3]]
+    quat = [round(n, 5) for n in quat]
+    euler = euler_from_quaternion(quat[0], quat[1], quat[2], quat[3])
+    euler = [round(n, 5) for n in euler]
 
-    return trans, rot_mat, quat
+    return trans, rot_mat, quat, euler
 
 
-
-###################################################
+##################################################
 def main():
-
+    
     panda_chain = get_chain()
 
     joint_positions = [0.0, -pi/8, 0.0, -5*pi/8, 0.0, pi/2, pi/4]  # example joint positions
 
-    trans, rot_mat, quat = compute_fk(joint_positions, panda_chain)
+    trans, rot_mat, quat, euler = compute_fk(joint_positions, panda_chain)
 
     # Output the position and orientation of the end effector
     print("Translation:", trans)
     print("Rotation Matrix:\n", rot_mat)
     print("Rotation quaternion:\n", quat)
+    print("Rotation Euler:\n", euler)
 
 
 
