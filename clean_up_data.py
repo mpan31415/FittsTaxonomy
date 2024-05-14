1f57beec12d592ed3733e4038eaa9e882a7d5f9f
mpan31415 <michael.pan31415@gmail.com>
2024-05-14 07:20:31 +0000

    add axes directions and gripper states

--- clean_up_data.py
+++ clean_up_data.py
@@ -4,9 +4,33 @@ from pandas import read_csv, DataFrame
 from os import getcwd
 from re import search, IGNORECASE
 from ast import literal_eval
+from numpy import array, matmul
 
 
 NUM_PARTICIPANTS = 10
+
+THRES1 = 0.001
+ID_DIFF = 35
+
+
+##################################################
+def generate_gripper_state_lst(gr):
+    
+    open_width = gr[0]
+    grasp_start_index = next(i for i in range(len(gr)) if open_width-gr[i] > THRES1)
+    grasp_finish_index = grasp_start_index + ID_DIFF
+    # print("\nGrasp performed between indexes %d and %d" % (grasp_start_index, grasp_finish_index))
+    
+    gr_state_list = []
+    for i in range(len(gr)):
+        if i < grasp_start_index:
+            gr_state_list.append("Empty")
+        elif grasp_start_index <= i < grasp_finish_index:
+            gr_state_list.append("Grasping")
+        else:
+            gr_state_list.append("Done")
+            
+    return gr_state_list
 
 
 ##################################################
@@ -42,7 +66,24 @@ def clean_data(part_id, task_id, trial_id, operation_type, panda_chain):
     ry_list = []
     rz_list = []
     
+    xx_list = []
+    xy_list = []
+    xz_list = []
+    
+    yx_list = []
+    yy_list = []
+    yz_list = []
+    
+    zx_list = []
+    zy_list = []
+    zz_list = []
+    
     positions = raw_df['positions'].tolist()
+    
+    # define the directions of the x, y, z, axes
+    x_dir = array([1, 0, 0])
+    y_dir = array([0, 1, 0])
+    z_dir = array([0, 0, 1])
     
     # loop through all recorded positions
     for pos_vec in positions:
@@ -58,6 +99,11 @@ def clean_data(part_id, task_id, trial_id, operation_type, panda_chain):
         # calculate forward kinematics
         trans, rot_mat, quat, euler = compute_fk(joint_positions, panda_chain)
         
+        # calculate axes directions (in original x, y, z basis vectors)
+        x_axis_dir = matmul(rot_mat, x_dir)
+        y_axis_dir = matmul(rot_mat, y_dir)
+        z_axis_dir = matmul(rot_mat, z_dir)
+        
         tx_list.append(trans[0])
         ty_list.append(trans[1])
         tz_list.append(trans[2])
@@ -71,9 +117,28 @@ def clean_data(part_id, task_id, trial_id, operation_type, panda_chain):
         ry_list.append(euler[1])
         rz_list.append(euler[2])
         
+        xx_list.append(x_axis_dir[0])
+        xy_list.append(x_axis_dir[1])
+        xz_list.append(x_axis_dir[2])
+        
+        yx_list.append(y_axis_dir[0])
+        yy_list.append(y_axis_dir[1])
+        yz_list.append(y_axis_dir[2])
+        
+        zx_list.append(z_axis_dir[0])
+        zy_list.append(z_axis_dir[1])
+        zz_list.append(z_axis_dir[2])
+        
+        
+    # use the gripper width list to generate grasp state list
+    gr_state_list = generate_gripper_state_lst(gr_list)
+    
+    
+    # generate dictionary for dataframe
     df_dict = {
         "time": times_from_start,
         "gr": gr_list,
+        "gr_state": gr_state_list,
         "tx": tx_list,
         "ty": ty_list,
         "tz": tz_list,
@@ -83,7 +148,16 @@ def clean_data(part_id, task_id, trial_id, operation_type, panda_chain):
         "qw": qw_list,
         "qx": qx_list,
         "qy": qy_list,
-        "qz": qz_list
+        "qz": qz_list,
+        "xx": xx_list,
+        "xy": xy_list,
+        "xz": xz_list,
+        "yx": yx_list,
+        "yy": yy_list,
+        "yz": yz_list,
+        "zx": zx_list,
+        "zy": zy_list,
+        "zz": zz_list
     }
     new_df = DataFrame(df_dict)
     
